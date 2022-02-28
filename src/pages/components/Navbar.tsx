d42737a0fbe3326df21a286fcd93432d4f3ede8c
Ramon Pereira <ramonpereira@MacBook-Pro-Ramon.local>
2022-02-28 00:11:28 -0300

    first commit - upload

---
+++ src/pages/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+   return (
+      <div>
+         <p>Hey fellas this is my first page!</p>
+      </div>
+   )
+} 
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="bg-slate-700 text-blue-200 bg-blend-normal">
+        <small>Hey guys this is my portfolio!</small>
+      </div>
+    </>
+    
+  )
+}
